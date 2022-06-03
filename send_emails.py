6fb4bd7e377c7af2d507cdf7f0db8241b5d00418
Kevio McClellan <57043715+KTheProgrammer@users.noreply.github.com>
2022-06-03 16:29:40 -0500

    Update send_emails.py

--- send_emails.py
+++ send_emails.py
@@ -3,8 +3,11 @@ import smtplib
 my_email = "your_email@gmail.com"
 password = "your_password"
 
-connect = smtplib.SMTP("smtp.gmail.com")
-connect.starttls()
-connect.login(user=my_email, password=password)
-connect.sendmail(from_addr=my_email, to_addrs="someone_email@yahoo.com", msg="Hello")
-connect.close()
+with smtplib.SMTP("smtp.gmail.com") as connect:
+  connect.starttls()
+  connect.login(user=my_email, password=password)
+  connect.sendmail(
+    from_addr=my_email, 
+    to_addrs="someone_email@yahoo.com", 
+    msg="Subject:Hello\n\nThis is the body of the email.")
+  
